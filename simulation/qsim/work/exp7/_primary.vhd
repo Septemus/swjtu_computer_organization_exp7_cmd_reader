f8d219794a2db3efed14d513f5cb8861dc16ffc5
joe <musketeerdt@gmail.com>
2023-05-23 10:34:28 +0800

    for simulate

--- simulation/qsim/work/exp7/_primary.vhd
+++ simulation/qsim/work/exp7/_primary.vhd
@@ -5,6 +5,7 @@ entity exp7 is
         out_num         : out    vl_logic_vector(15 downto 0);
         clk             : in     vl_logic;
         pc_clr          : in     vl_logic;
+        DATA_INPUT      : in     vl_logic_vector(7 downto 0);
         M               : in     vl_logic_vector(1 downto 0);
         pc1             : out    vl_logic_vector(7 downto 0)
     );
--- simulation/qsim/work/exp7_vlg_sample_tst/_primary.vhd
+++ simulation/qsim/work/exp7_vlg_sample_tst/_primary.vhd
@@ -3,6 +3,7 @@ use verilog.vl_types.all;
 entity exp7_vlg_sample_tst is
     port(
         clk             : in     vl_logic;
+        DATA_INPUT      : in     vl_logic_vector(7 downto 0);
         M               : in     vl_logic_vector(1 downto 0);
         pc_clr          : in     vl_logic;
         sampler_tx      : out    vl_logic
